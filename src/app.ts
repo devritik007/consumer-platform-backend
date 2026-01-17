f8bff818ed9ae120557ae0cf27866b8fc37a1a66
nikhil022 <niksocial2@gmail.com>
2026-01-17 14:40:46 +0530

    final changes

--- src/app.ts
+++ src/app.ts
@@ -9,16 +9,12 @@ const app: Express = express();
 
 app.use(express.json());
 
-const allowedOrigins = env.ALLOWED_ORIGINS?.split(",").map((o) => o.trim()) || [
-  "http://localhost:3000",
-];
+const allowedOrigins = ["http://localhost:3000", "http://localhost:3001"];
 
 app.use(
   cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-
-      if (allowedOrigins.includes(origin)) {
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
         callback(null, true);
       } else {
         callback(new Error("Not allowed by CORS"));
